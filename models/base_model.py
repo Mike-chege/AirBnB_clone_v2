8c491fc9d6c6afb672de00dc211e9eb9ee804bd6
Mike-chege <mikechege93@gmail.com>
2023-08-23 09:22:29 -0700

    The BaseModel class which all other classes will inherit from

---
+++ models/base_model.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""
+The BaseModel module
+"""
+import models
+from uuid import uuid4
+from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import String
+
+Base = declarative_base()
+
+
+class BaseModel:
+    """
+    The Class BaseModel Definition
+    Implements the created_at and the updated_at for the sqlalchemy
+    Datetime
+    """
+
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializng
+        A new BaseModel.
+        """
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+
+    def save(self):
+        """
+        Method save
+        Save in updated_at with the current datetime
+        """
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        Method to_dict
+        Returns dictionary representation of the BaseModel instance
+        """
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = str(type(self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict.pop("_sa_instance_state", None)
+        return my_dict
+
+    def delete(self):
+        """
+        Method delete
+        Deletes the an instance from storage
+        """
+        models.storage.delete(self)
+
+    def __str__(self):
+        """
+        Method __str__
+        Returns the string representation of the BaseModel instance
+        """
+        d = self.__dict__.copy()
+        d.pop("_sa_instance_state", None)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, d)
