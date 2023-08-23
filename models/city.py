bb0bf961c779744a047275eb26e55433c072a2db
Mike-chege <mikechege93@gmail.com>
2023-08-23 09:27:52 -0700

    Implementing the city class inherits from the Base class & changed the string value

---
+++ models/city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+The City class Module
+"""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+
+
+class City(BaseModel, Base):
+    """
+    Cass City
+    The city representation for the MySQL database
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
