822d30d1729552293b49f1257732450a0fdb1cee
Mike-chege <mikechege93@gmail.com>
2023-08-23 02:30:02 -0700

    Setting up a MySQL server for the project(second one)

---
+++ setup_mysql_test.sql
@@ -0,0 +1,16 @@
+-- Creating a MySQL Database for the project
+
+-- Create the database hbnb_test_db if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create the user hbnb_test if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges on hbnb_test_db to the user hbnb_test
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to the user hbnb_test
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
