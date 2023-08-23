97e4c025bfe5b669c34023373f09e49fb482b13a
Mike-chege <mikechege93@gmail.com>
2023-08-23 09:55:10 -0700

    Using a many to many relationship by adding another table

---
+++ models/review.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+The Class Review Module
+"""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+
+
+class Review(BaseModel, Base):
+    """
+    The Review Class
+    Defines the review for the MySQL database
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
