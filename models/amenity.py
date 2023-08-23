9a77098f0700cade27b9ae4ddc94f94c779534fd
Mike-chege <mikechege93@gmail.com>
2023-08-23 10:04:17 -0700

    Added the atrribute place_amenities which represents the many to many relationship

---
+++ models/amenity.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+The Class Amenity Module
+"""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+
+
+class Amenity(BaseModel, Base):
+    """
+    The Class Amenity
+    Defines the Amenity Class for the MySQL database
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
