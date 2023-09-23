9881a3ea30451fc4acd1d132bede8ee2806e8beb
Mikee-chege <mikechege93@gmail.com>
2023-09-23 02:10:45 -0700

    Added a new function to display a HTML page only if n is an integer/fixed typos

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     """
@@ -64,7 +65,7 @@ def number_template(n):
     return render_template("5-number.html", n=n)
 
 
-@app.route("/number_odd_or_even/<int:n>",strict_slashes=False)
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
     """
     Prints HTML page
