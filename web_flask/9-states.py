8b6e83dedcd61b30fe92ecbb2ccb5ccc7d8f6c4d
Mikee-chege <mikechege93@gmail.com>
2023-09-23 03:28:11 -0700

    Listing all states objects present in the DBStorage

---
+++ web_flask/9-states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+def list_state_objects():
+    """
+    Prints HTML page
+    With a list of all States
+    """
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def list_state_id(id):
+    """
+    Prints HTML page
+    With id information if present
+    """
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+        return render_template("9-states.html")
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exc):
+    """
+    Closes and removes the current SQLAchemy session in the file storage
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
